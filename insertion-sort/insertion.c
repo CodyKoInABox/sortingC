ed1f06a3ab07f72f03f4a7ccd91abdd5d23d1850
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2024-01-14 16:55:52 +0530

    Update insertion.c
    Replaced i with 1

--- insertion-sort/insertion.c
+++ insertion-sort/insertion.c
@@ -3,11 +3,11 @@ void insertion(int array[], int length){
     for(int i = 1; i < length; i++){
         
         int pivot = array[i];
-        int j = i - i;
+        int j = i - 1;
 
         while(j >= 0 && array[j] > pivot){
 
-            array[j + i] = array[j];
+            array[j + 1] = array[j];
             j--;
         }
         
